ed59b5e125b3e3810d2fcd63c47727be4251adfd
imtiajlinux <imtiaj.linux@gmail.com>
2023-08-01 18:32:46 +0600

    fixing

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,6 +2,11 @@ import "./HeroImgStyle.css";
 import React from "react";
 import IntroImg from "../asscet/home-upper.jpg";
 import { Link } from "react-router-dom";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import Myphoto from "../asscet/my photo in haji.png";
 
 const HeroImg = () => {
   const test = "{ front-end web developer }";
@@ -11,6 +16,7 @@ const HeroImg = () => {
         <img className="into-img" src={IntroImg} alt="heroImgHome" />
         <div className="content">
           {/* <p>Hi, I AM A FRONT_END WEB LEARNER</p> */}
+          <img src={Myphoto} alt="photo" className="myphotoo" />
           <p>
             hi ,{" "}
             <span
@@ -23,7 +29,12 @@ const HeroImg = () => {
             {"  "}
           </p>
           <h1>{test}</h1>
-          <p>html . css . javascript . react</p>
+          <p>
+            <FaHtml5 className="hero-icon" /> .{" "}
+            <FaCss3Alt className="hero-icon" /> .{" "}
+            <SiJavascript className="hero-icon" /> .{" "}
+            <FaReact className="hero-icon" />
+          </p>
           <div style={{ margin: "25px 0" }}>
             <Link className="btn" to="/project">
               Projects
